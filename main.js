d91f87dc4d0be59d4283a06ef14df395d9b32a06
vargasaldrin <vargasero@gmail>
2020-09-29 21:18:14 +0800

    updated content

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/***  Variables ***/
+/***  Variables and Data***/
 const speed = 100;
 const main = document.getElementById('main_content');
 const about = document.getElementById('about');
@@ -7,27 +7,29 @@ const jscript = document.getElementById('javascript');
 const webtech = document.getElementById('webtech');
 const description = document.getElementById('description');
 const slideDown = document.getElementById('slideDown');
-const mq = window.matchMedia("(max-width: 576px");
+const mediaSize = window.matchMedia("(max-width: 576px)");
 const closing = document.getElementsByClassName('fa-times');
-const list = [
-    'Front-End Developer',
-    'Programmer',
+const blackbox = document.getElementsByClassName('box1')
+
+const titles =  [
+    'Front-end Developer',
+    'Javascript Developer',
+    'Lifelong Learner',
     'React Developer',
-    'SEO Enthusiast',
     'Boardgame Enthusiast',
-    'Pug Lover',
-    'Funny Man',
-    ':)'
+    'Loves Pugs',
+    'Critical Thinker',
+    'Detail Oriented',
+    'Ball is life',
+    '"Walang matigas na tinapay sa mainit na kape"',
+    'Insert Text Here'
 ]
-const mediaSize = window.matchMedia("(max-width: 576px");
+
 
 /***  Functions ***/
-
-
-
 inserter = (e) => {
-    const txt = e.target.title.toString();
-    e.target.innerHTML = txt;
+    const text = e.target.title.toString();
+    e.target.innerHTML = text;
     e.target.style.cursor = 'pointer';       
 };
 
@@ -39,169 +41,114 @@ deleter = (e) => {
     webtech.innerHTML= '<i class="fas fa-cogs fa-2x"></i>';
 };
 
-slideIn = (e) => {
-    let toSlide = document.getElementById(e)
-    var pos = 0;
+open = (value) => {
+    const toSlide = document.getElementById(value)
+    
+    let pos = 0;
     const frame = () => {
-        if(mq.matches) {
+        if(mediaSize.matches) {
             if (pos == -600) {
-                clearInterval(interv);
+                clearInterval(loop);
             } else {
                 pos -= 100; 
                 toSlide.style.top = pos + 'px'; 
             }
         } else {
             if (pos == -700) {
-                clearInterval(interv);
+                clearInterval(loop);
             } else {
                 pos -= 100; 
                 toSlide.style.top = pos + 'px'; 
             }
         }
-        
-    }  
-    var interv = setInterval(frame, 100);
+    } 
+    const loop = setInterval(frame, 100);
 };
 
-slideOut = (e) => {
-    let toSlide = document.getElementById(e);
+close = (value) => {
+    const toSlide = document.getElementById(value)
     let pos;
-    if (mq.matches){
+    if (mediaSize.matches){
         pos = -600;
     } else {
         pos = -700;
     }
     const frame = () => {
         if (toSlide.style.top == '0px' || !toSlide.style.top) {
-          clearInterval(interv);
+          clearInterval(loop);
         } else {
             pos += 100; 
             toSlide.style.top = pos + 'px'; 
         } 
-    }  
-    var interv = setInterval(frame, 100);
+    }
+    const loop = setInterval(frame, 100);
 };
 
 /***  Event Listeners ***/
-main.addEventListener('mouseover', (e) => {
-    if (e.target == about) {
-        inserter(e);
-    } else if (e.target == htmlcss) {
-        inserter(e)
-    } else if (e.target == jscript) {
-        inserter(e)
-    } else if (e.target == webtech) {
-        inserter(e)
-    }
-});
-
-main.addEventListener('mouseout', (e) => {
-    if (e.target == about){
-        deleter(e);
-    } else if (e.target == htmlcss){
-        deleter(e);
-    } else if (e.target == jscript){
-        deleter(e);
-    } else if (e.target == webtech){
-        deleter(e);
-    }
-}); 
-
-main.addEventListener('click', (e) => {
-    if(e.target == about) {
-        slideIn('about2');
-        slideOut('htmlcss2');
-        slideOut('javascript2');
-        slideOut('webtech2');
-    } else if(e.target == htmlcss) {
-        slideIn('htmlcss2');
-        slideOut('about2');
-        slideOut('javascript2');
-        slideOut('webtech2');
-    } else if(e.target == javascript) {
-        slideIn('javascript2');
-        slideOut('htmlcss2');
-        slideOut('about2');
-        slideOut('webtech2');
-    } else if(e.target == webtech) {
-        slideIn('webtech2');
-        slideOut('htmlcss2');
-        slideOut('javascript2');
-        slideOut('about2');
-    } else if(e.target == closing) {
-        slideOut('about2');
-    }
-});
+for(let i = 0; i < blackbox.length; i++) {
+    console.log(blackbox[i])
+    blackbox[i].addEventListener('mouseover', (e) => inserter(e))
+    blackbox[i].addEventListener('mouseout', (e) => deleter(e))
+    blackbox[i].addEventListener('click', (e) => {
+        if(e.target == about) {
+            open('about2');
+            close('htmlcss2');
+            close('javascript2');
+            close('webtech2');
+        } else if(e.target == htmlcss) {
+            open('htmlcss2');
+            close('about2');
+            close('javascript2');
+            close('webtech2');
+        } else if(e.target == javascript) {
+            open('javascript2');
+            close('htmlcss2');
+            close('about2');
+            close('webtech2');
+        } else if(e.target == webtech) {
+            open('webtech2');
+            close('htmlcss2');
+            close('javascript2');
+            close('about2');
+        } else if(e.target == closing) {
+            close('about2');
+        }
+    })
+}
 
 document.addEventListener('DOMContentLoaded',function(e){
-    // List of sentences
-    var _CONTENT = [ 
-        'Front-end Developer',
-        'Javascript Developer',
-        'Lifelong Learner',
-        'React Developer',
-        'Boardgame Enthusiast',
-        'Loves Pugs',
-        'Critical Thinker',
-        'Detail Oriented',
-        'Ball is life',
-        '"Walang matigas na tinapay sa mainit na kape"'
-    ];
+    let index = 0;
+    let letter = 0;
+    let interval;
+    const description = document.getElementById("description");
 
-    // Current sentence being processed
-    var _PART = 0;
-
-    // Character number of the current sentence being processed 
-    var _PART_INDEX = 0;
-
-    // Holds the handle returned from setInterval
-    var _INTERVAL_VAL;
-
-    // Element that holds the text
-    var _ELEMENT = document.getElementById("description");
-
-    // Implements typing effect
     function Type() { 
-    var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX++;
-
-    // If full sentence has been displayed then start to delete the sentence after some time
-    if(text === _CONTENT[_PART]) {
-        clearInterval(_INTERVAL_VAL);
-        setTimeout(function() {
-            _INTERVAL_VAL = setInterval(Delete, 50);
-        }, 1000);
+        var text =  titles[index].substring(0, letter + 1);
+        description.innerHTML = text;
+        letter++;
+        if(text === titles[index]) {
+            clearInterval(interval);
+            setTimeout(function() {
+                interval = setInterval(Delete, 50);
+            }, 2000);
+        }
     }
-    }
-
-    // Implements deleting effect
     function Delete() {
-    var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-    _ELEMENT.innerHTML = text;
-    _PART_INDEX--;
-
-    // If sentence has been deleted then start to display the next sentence
-    if(text === '') {
-        clearInterval(_INTERVAL_VAL);
-
-        // If last sentence then display the first one, else move to the next
-        if(_PART == (_CONTENT.length - 1))
-            _PART = 0;
-        else
-            _PART++;
-        _PART_INDEX = 0;
-
-        // Start to display the next sentence after some time
-        setTimeout(function() {
-            _INTERVAL_VAL = setInterval(Type, 100);
-        }, 200);
-    }
+        var text =  titles[index].substring(0, letter - 1);
+        description.innerHTML = text;
+        letter--;
+        if(text === '') {
+            clearInterval(interval);
+            if(index == (titles.length - 1))
+                index = 0;
+            else
+                index++;
+                letter = 0;
+            setTimeout(function() {
+                interval = setInterval(Type, 100);
+            }, 1000);
+        }
     }
 
-    // Start the typing effect on load
-    _INTERVAL_VAL = setInterval(Type, 100);
+    interval = setInterval(Type, 100);
   });
-
-
-  /* RESUME' ANIMATION*/
