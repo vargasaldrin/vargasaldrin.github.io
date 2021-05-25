104ae6f521bee0dd307292aebf914920d7d61d30
Aldrin Vargas <aldrin@symph.co>
2021-05-25 11:03:44 +0800

    Removed files, page redirect

--- data/contact.js
+++
@@ -1,9 +0,0 @@
-const contact = {
-  email: "vargasaldrin0805@gmail.com",
-  location: "Metro Manila, Philippines",
-  phone: "+63-965-327-3935",
-  facebook: "https://www.facebook.com/vargas0805/",
-  github: "https://github.com/vargasaldrin",
-};
-
-export default contact;
